fdfc53d3c91150299db614e9b7cabb188bdf95ff
ON10N <zhyc9de@hotmail.com>
2018-07-12 20:13:50 +0800

    加入.d.ts

---
+++ types/index.ts
@@ -0,0 +1,5 @@
+/// <reference path="../node_modules/@minapp/wx/typing/app.d.ts" />
+/// <reference path="../node_modules/@minapp/wx/typing/behavior.d.ts" />
+/// <reference path="../node_modules/@minapp/wx/typing/component.d.ts" />
+/// <reference path="../node_modules/@minapp/wx/typing/page.d.ts" />
+/// <reference path="../node_modules/@minapp/wx/wxp.d.ts" />
